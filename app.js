fda93b16494d93e895e21159ffa4ea9769e68f24
Anthony Bobongo <anthony.bobongo@gmail.com>
2020-06-11 13:00:06 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const mins = fullDate.getMinutes();
+const secs = fullDate.getSeconds();
+
+document.getElementById("hour").innerHTML = hours;
+document.getElementById("minute").innerHTML = mins;
+document.getElementById("seconds").innerHTML = secs;
+
+function clock() {
+  const fullDate = new Date();
+  let hours = fullDate.getHours();
+  let mins = fullDate.getMinutes();
+  let secs = fullDate.getSeconds();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = ":" + mins;
+  document.getElementById("seconds").innerHTML = ":" + secs;
+}
+setInterval(clock, 100);
